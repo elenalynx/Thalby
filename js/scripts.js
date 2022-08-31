d19e9b7210747f2aa62954c00bf0af7d8462627e
elenalynx <elnlynx@gmail.com>
2022-09-01 00:46:52 +0300

    created jquery ui dialog animated for Search

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,46 @@
 $(document).ready(function () {
 
+    $(function () {
+        // "use strict"
+
+        $("#dialog").dialog({
+            autoOpen: false,
+            show: {
+                effect: "blind",
+                duration: 1000
+            },
+            hide: {
+                effect: "blind",
+                duration: 1000
+            },
+// НЕ ПОЛУЧАЕТСЯ ОЧИСТИТЬ ПОЛЕ ВВОДА ПРИ ЗАКРЫТИИ ВСПЛЫВАЮЩЕГО ОКНА (попытки)
+            // select: function (event, ui) {
+            //     $(this).val('');
+            //     return false;
+            // }
+            // close: function () {
+            //     $("#dialog").closest('form');
+            // }
+            // button: {
+            //     "btn_search": function () {
+            //         $(this).dialog('post');
+            //     }
+            // }
+        });
+
+        $("#opener").on("click", function () {
+            $("#dialog").dialog("open");
+        });
+
+
+        // НЕ ПОЛУЧАЕТСЯ ОЧИСТИТЬ ПОЛЕ ВВОДА ПРИ ЗАКРЫТИИ ВСПЛЫВАЮЩЕГО ОКНА
+
+        $('.ui-dialog .ui-dialog-titlebar-close').bind('click', function () {
+            $("#dialog").closest('form').find('input[type=text], textarea').val('');
+        });
+
+    });
+
     $('.owl-carousel').owlCarousel({
         loop: true,
         margin: 0,
@@ -13,7 +54,7 @@ $(document).ready(function () {
             0: {
                 items: 1
             },
-            768: {
+            700: {
                 items: 2,
             },
             860: {
